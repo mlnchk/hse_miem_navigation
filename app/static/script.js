446675a42a16079e1c07ea8325b0ffc6478dd250
mlnchk <danil.mlnchk@gmail.com>
2017-05-13 05:49:34 +0300

    another one

--- app/static/script.js
+++ app/static/script.js
@@ -8,18 +8,41 @@ $(document).ready(function(){
     var startFloor = Math.floor(start / 100);
     var finishFloor = Math.floor(finish / 100);
 
-    if (startFloor != finishFloor) {
+    console.log($('#' + start).parent()[0]);
+    console.log($('#' + finish).parent()[0]);
+
+    var lol = $('#path')[0].innerText;
+    lol = lol.slice(1, lol.length - 1).replace(/\s+/g,'');
+    lol = lol.split(',');
+    console.log(lol);
+
+    if ($('#' + start).parent()[0] == $('#' + finish).parent()[0]) {
+        $('#' + start).parent().removeClass('st0');
+        $('svg').children().children().css('opacity', '0.2');
+        $('#main').children().css('opacity', '1');
+        $('#' + start).css('opacity', '1');
+        $('#' + finish).css('opacity', '1');
+    } else {
         $('#next-step').removeClass('hidden');
+        $('#' + start).parent().removeClass('st0');
+        $('svg').children().children().css('opacity', '0.2');
+        $('#main').children().css('opacity', '1');
+        $('#' + start).css('opacity', '1');
+
+        for (var i = 0; i < lol.length; i++) {
+            console.log(lol[i]);
+            if (lol[i] == "'LE'") {
+                console.log('yes');
+                $('#LE').css('opacity', '1');
+            }
+        }
+
+        $('#next-step').click(function () {
+            $('#' + start).parent().addClass('st0');
+            $('#' + finish).parent().removeClass('st0');
+            $('#' + finish).css('opacity', '1');
+        })
     }
-
-    $('#' + start).parent().removeClass('st0');
-    $('#' + finish).parent().removeClass('st0');
-
-    $('svg').children().children().css('opacity', '0.2');
-    $('#main').children().css('opacity', '1');
-
-    $('#' + start).css('opacity', '1');
-    $('#' + finish).css('opacity', '1');
 
 });
 
