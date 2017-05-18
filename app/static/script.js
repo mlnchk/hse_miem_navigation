df35ec82914a2de48fd08021574c204845d2d399
mlnchk <danil.mlnchk@gmail.com>
2017-05-18 23:07:15 +0300

    some changes

--- app/static/script.js
+++ app/static/script.js
@@ -1,48 +1,97 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
     var start = $('#start-data').text(), finish = $('#finish-data').text();
 
-    console.log(Math.floor(start / 100));
-    console.log(Math.floor(finish / 100));
 
-    var startFloor = Math.floor(start / 100);
-    var finishFloor = Math.floor(finish / 100);
-
-    console.log($('#' + start).parent()[0]);
-    console.log($('#' + finish).parent()[0]);
+    if (start != 'undefined' && finish != 'undefined') {
+        $('#stage-1').addClass('st0');
+        $('#stage-1a').addClass('st0');
+    }
 
     var lol = $('#path')[0].innerText;
-    lol = lol.slice(1, lol.length - 1).replace(/\s+/g,'');
+    lol = lol.slice(1, lol.length - 1).replace(/\s+/g, '');
     lol = lol.split(',');
     console.log(lol);
 
-    if ($('#' + start).parent()[0] == $('#' + finish).parent()[0]) {
-        $('#' + start).parent().removeClass('st0');
+    var startFloor = $('#' + start).parent(), finishFloor = $('#' + finish).parent();
+
+    if (startFloor[0] == finishFloor[0]) {
         $('svg').children().children().css('opacity', '0.2');
         $('#main').children().css('opacity', '1');
+        startFloor.removeClass('st0');
         $('#' + start).css('opacity', '1');
         $('#' + finish).css('opacity', '1');
     } else {
         $('#next-step').removeClass('hidden');
-        $('#' + start).parent().removeClass('st0');
         $('svg').children().children().css('opacity', '0.2');
         $('#main').children().css('opacity', '1');
         $('#' + start).css('opacity', '1');
+        startFloor.removeClass('st0');
 
-        for (var i = 0; i < lol.length; i++) {
-            console.log(lol[i]);
-            if (lol[i] == "'LE'") {
-                console.log('yes');
-                $('#LE').css('opacity', '1');
+        var temp = 0;
+
+        // лифты лестницы
+        if (startFloor[0].id.length < finishFloor[0].id.length) {
+            for (var i = 0; i < lol.length; i++) {
+                if (lol[i] == "'LE'" || lol[i] == "'LW'" || lol[i] == "'LN'" || lol[i] == "'LS'" || lol[i] == "'Lu'" || lol[i] == "'LWa'") {
+                    $('#' + lol[i].replace("'", "").replace("'", "")).css('opacity', '1');
+                    // temp += 1;
+                    break
+
+                }
             }
+        } else if (startFloor[0].id.length > finishFloor[0].id.length) {
+            $('#LWa').css('opacity', '1');
         }
 
-        $('#next-step').click(function () {
-            $('#' + start).parent().addClass('st0');
-            $('#' + finish).parent().removeClass('st0');
-            $('#' + finish).css('opacity', '1');
-        })
-    }
 
+        $('#next-step').click(function () {
+            if (startFloor[0].id.length == finishFloor[0].id.length) {
+                startFloor.addClass('st0');
+                finishFloor.removeClass('st0');
+                $('#' + finish).css('opacity', '1');
+                $('#next-step').addClass('hidden');
+            } else if (startFloor[0].id.length > finishFloor[0].id.length) {
+                for (var i = 0; i < lol.length; i++) {
+                    if (lol[i] == "'LE'" || lol[i] == "'LW'" || lol[i] == "'LN'" || lol[i] == "'LS'" || lol[i] == "'Lu'") {
+                        $('#' + lol[i].replace("'", "").replace("'", "")).css('opacity', '1');
+                        break
+                    }
+                }
+
+                startFloor.addClass('st0');
+                $('#stage-1').removeClass('st0');
+                $('#center').css('opacity', '1');
+                $('#stage-1a').removeClass('st0');
+                $('#LWa').css('opacity', '1');
+
+                $('#next-step').click(function () {
+                    $('#LWa').css('opacity', '0');
+                    // $('#' + lol[i].replace("'", "").replace("'", "")).css('opacity', '0');
+                    $('#stage-1').addClass('st0');
+                    $('#stage-1a').addClass('st0');
+                    finishFloor.removeClass('st0');
+                    $('#' + finish).css('opacity', '1');
+                    $('#next-step').addClass('hidden');
+                });
+            } else if (startFloor[0].id.length > finishFloor[0].id.length) {
+
+                startFloor.addClass('st0');
+                $('#stage-1a').removeClass('st0');
+                $('#center').css('opacity', '1');
+                $('#stage-1').removeClass('st0');
+
+                $('#next-step').click(function () {
+                    $('#stage-1').addClass('st0');
+                    finishFloor.removeClass('st0');
+                    $('#' + finish).css('opacity', '1');
+                    $('#next-step').addClass('hidden');
+                });
+            }
+            // $('#stage-1a').addClass('st0');
+
+        });
+
+    }
 });
 
